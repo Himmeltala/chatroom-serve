151dae721d890c1819b78638a42f546377c9b7fb
shiramashiro <zhengrenfu@outlook.com>
2022-08-12 22:00:20 +0800

    初始化仓库

---
+++ src/socketio/index.js
@@ -0,0 +1,20 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
+
+const httpServer = createServer();
+const server = new Server(httpServer, { cors: { origin: true, credentials: true } });
+
+/**
+ * 监听客户端连接
+ */
+server.on("connection", function (socket) {
+
+  socket.on("sending", function (e) {
+    socket.broadcast.emit("broadcast", e);
+  });
+
+});
+
+instrument(server, { auth: false });
+httpServer.listen(3000);
