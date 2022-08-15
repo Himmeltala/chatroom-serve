b6012966258001284b4222691e387e41956570b3
shiramashiro <zhengrenfu@outlook.com>
2022-08-16 02:27:59 +0800

    解决 Cookie 不能保存在 Application 中的问题

--- src/apis/userApi.ts
+++ src/apis/userApi.ts
@@ -4,7 +4,17 @@ import { queryUserAllByUname } from "../database/userDB";
 /**
  * 通过用户名和密码查询用户所有字段
  */
-server.post("/query/user/by/key", async (req, res) => {
-  let result = await queryUserAllByUname(req.body.username, req.body.password);
-  res.send(result);
+server.post("/login", async (req, res) => {
+  let data = await queryUserAllByUname(req.body.username, req.body.password);
+  let status = 404;
+  if ( data.length > 0 ) {
+
+    res.cookie("USERID", data[0].id, { maxAge: 1000 * 60 * 60 * 24 });
+    status = 200;
+  }
+  res.send({ data, status });
+});
+
+server.get("/get/cookie", (req, res) => {
+  res.send(req.cookies);
 });
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const server = express();
 
-server.use(cors());
+server.use(cors({ credentials: true, origin: true }));
+server.use(cookieParser());
 server.use(express.json()); // 解析 application/json
 server.use(express.urlencoded({ extended: true })); // 解析 application/x-www-form-urlencoded
 
